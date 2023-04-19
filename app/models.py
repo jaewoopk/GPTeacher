c65b238d134c36001d9262a17497ae5e495508c2
jaewoopk <jaewoopk000@naver.com>
2023-04-19 17:59:40 +0900

    Day 1 Making Model about User, Sentence data and Migrate it

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model) :
+    id = models.CharField(primary_key=True, max_length=50)  # 유저 ID
+    password = models.CharField(max_length=20)              # 유저 비밀번호
+
+class SentenceData(models.Model) :
+    sentenceNum = models.IntegerField(primary_key=True)     # 문장 번호
+    contents = models.CharField(max_length=200)             # 문장 내용
+    level = models.IntegerField()                           # 문장 레벨
+    hint = models.CharField(max_length=100)                 # 문장 힌트
+    blankWord = models.CharField(max_length=50)             # 빈칸 단어
+    blankPos = models.IntegerField()                        # 빈칸 위치
+    ansKor = models.CharField(max_length=400)               # 정답 한글 문장
