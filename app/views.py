2b2a69718954c9dfe70faa7b38c964fb93a5ce54
jaewoopk <jaewoopk000@naver.com>
2023-04-19 17:49:22 +0900

    Day 1 Revise views and urls (just examples)

--- app/views.py
+++ app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 
-# Create your views here.
+# 현재 views는 기본 예시
+
+# --> FBV : Function Based View = 함수 기반 뷰
+@api_view(['GET']) # Decorator -> 함수를 꾸미는 역할(함수에 대한 성격을 표시해주는 표기법)
+def HelloAPI(request) :
+    return Response("hello world!")
+
+# --> CBV : Class Based View = 클래스 기반 뷰
+# class HelloAPI(APIView) :
+#     def get(self, request) :
+#         return Response("hello wolrd")
