0b1e67ce19ec0448cd182e0c6ef45aa219504ac0
jaewoopk <jaewoopk000@naver.com>
2023-04-20 13:51:14 +0900

    Day 2 Add Models about functional table

--- app/models.py
+++ app/models.py
@@ -14,3 +14,19 @@ class SentenceData(models.Model) :
     blankWord = models.CharField(max_length=50)             # 빈칸 단어
     blankPos = models.IntegerField()                        # 빈칸 위치
     ansKor = models.CharField(max_length=400)               # 정답 한글 문장
+
+class UserStudyInfo(models.Model) :
+    id = models.CharField(foreign_key=True, max_length=50)  # 유저 ID
+    sentenceNum = models.IntegerField(foreign_key = True)   # 문장 번호
+    wrongNum = models.IntegerField()                        # 틀린 횟수
+    check = models.IntegerField()                           # 오답 횟수
+
+class UserGameInfo(models.Model) :
+    id = models.CharField(foreign_key=True, max_length=50)  # 유저 ID
+    sentenceNum = models.IntegerField(foreign_key = True)   # 문장 번호
+    score = models.IntegerField()                           # 게임 점수
+
+class Ranking(models.Model) :
+    id = models.CharField(foreign_key=True, max_length=50)  # 유저 ID
+    userList = models.CharField(max_length=200)             # 유저 리스트
+    maxScore = models.IntegerField()                        # 게임 최고 점수
