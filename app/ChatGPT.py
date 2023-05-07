8a5ad04c75a1e19a046f8bc133545defaeaedc79
wo658 <whdgns2347@gmail.com>
2023-05-07 14:10:17 +0900

    문장2

--- app/ChatGPT.py
+++ app/ChatGPT.py
@@ -3,7 +3,7 @@ import time
 import openpyxl
 # API 키
 
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = "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"
 
 openai.api_key = OPENAI_API_KEY
 
@@ -11,18 +11,18 @@ model = "gpt-3.5-turbo"
 
 messages = []
 # 엑셀 파일
-wb = openpyxl.Workbook()
+
+fpath = '/Users/KJH/Documents/GitHub/Capstone_6/excel/Sentence.xlsx'
+wb = openpyxl.load_workbook(fpath)
 # 워크시트
-ws = wb.create_sheet('문장모음')
-ws['A1'] = '문장'
-ws['B1'] = '정답'
-k=2
-for i in range(5):
+ws = wb['문장모음']
+k = 1
+for i in range(10):
     #1 보기없는 버전
-    content = "To study English,make 5 TOEIC English problems using the blanks.with answer for example problem is The company's goal is to _______ market share in the next fiscal year.Answer: increase"
+    content = "To study English,make 10 TOEIC problems using blank.with answer"
 
     messages.append({"role":"user", "content":content})
-    time.sleep(20)
+    time.sleep(10)
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages
@@ -30,14 +30,16 @@ for i in range(5):
 
     chat_response = completion.choices[0].message.content
 # chat_response 는 str 변수이다.
-    chat_response = chat_response.replace('Answer:', '.').replace('1.', '.').replace('2.', '.').replace('3.', '.').replace('4.', '.').replace('5.', '.')
+    chat_response = chat_response.replace('Answer:', '.').replace('-','.')
+    for N in range(10):
+        chat_response = chat_response.replace(str(N+1)+'.', '.')
     print(chat_response)
-    for j in range(5):
+    for j in range(10):
         ws['A'+str(k)] = chat_response.split('.')[j*3+1]
         ws['B'+str(k)] = chat_response.split('.')[3*(j+1)].replace(' ', '').replace('\n', '')
         k = k + 1
     #print(chat_response.split('.')[4])
     messages.append({"role": "assistant", "content": chat_response})
-    wb.save('/Users/KJH/Documents/GitHub/Capstone_6/excel/Sentence.xlsx')
+    wb.save('/Users/KJH/Documents/GitHub/Capstone_6/excel/Sentence2.xlsx')
 
 
---
+++ app/ChatGPT2.py
@@ -0,0 +1,69 @@
+import openai
+import time
+import openpyxl
+# API 키
+
+OPENAI_API_KEY = "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"
+
+openai.api_key = OPENAI_API_KEY
+
+model = "gpt-3.5-turbo"
+
+messages = [{"role": "system", "content" :"Make 10 fill-in-the-blank TOEFL vocabulary problems with 4 choices. provide answer."}]
+# 엑셀 파일
+
+fpath = '/Users/KJH/Documents/GitHub/Capstone_6/excel/Sentence2.xlsx'
+wb = openpyxl.load_workbook(fpath)
+# 워크시트
+ws = wb['문장모음']
+k = 101
+for n in range(100):
+    time.sleep(40)
+    k = k + 25
+    for i in range(5):
+        if n % 4 == 0:
+            content = "Make 10 fill-in-the-blank TOEFL vocabulary problems with 4 choices. provide answer."
+        elif n % 4 == 1:
+            content = "Make 10 fill-in-the-blank TOEIC vocabulary problems with 4 choices. provide answer."
+        elif n % 4 == 2:
+            content = "Make 10 fill-in-the-blank IELTS vocabulary problems with 4 choices. provide answer."
+        else:
+            content = "Make 10 fill-in-the-blank TEPS vocabulary problems with 4 choices. provide answer."
+        messages.append({"role": "user", "content": content})
+        time.sleep(30)
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages
+        )
+
+        chat_response = completion.choices[0].message.content
+    # chat_response 는 str 변수이다.
+        print(chat_response)
+        chat_response = chat_response.replace('Answer: ', '').replace('-', '').replace(')', '').replace('.','')
+        sentenceNum=0
+        for M in range(1,10):
+
+            if chat_response.split('\n')[M] != "":
+                if chat_response.split('\n')[M].find('__') != -1:
+                    sentenceNum = M
+        for N in range(10):
+            chat_response = chat_response.replace(str(N), '')
+
+        print(sentenceNum)
+        for j in range(10):
+            ws['A'+str(k)] = chat_response.split('\n')[0+sentenceNum*j]
+            if sentenceNum == 8:
+                ws['F'+str(k)] = chat_response.split('\n')[6+sentenceNum*j].split(' ')[0]
+            else:
+                ws['F' + str(k)] = chat_response.split('\n')[5 + sentenceNum * j].split(' ')[0]
+            ws['B'+str(k)] = chat_response.split('\n')[1+sentenceNum*j].split(' ')[1]
+            ws['C'+str(k)] = chat_response.split('\n')[2+sentenceNum*j].split(' ')[1]
+            ws['D'+str(k)] = chat_response.split('\n')[3+sentenceNum*j].split(' ')[1]
+            ws['E'+str(k)] = chat_response.split('\n')[4+sentenceNum*j].split(' ')[1]
+
+            k = k + 1
+        #print(chat_response.split('.')[4])
+        messages.append({"role": "assistant", "content": chat_response})
+        wb.save('/Users/KJH/Documents/GitHub/Capstone_6/excel/Sentence2.xlsx')
+
+
