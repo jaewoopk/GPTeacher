136a931501c9be0cb4df837806b85e4ea4120a3d
jiyoon <jyoon0640@gmail.com>
2023-05-14 15:47:49 +0900

    no message

--- app/static/app/js/joinscript.js
+++ app/static/app/js/joinscript.js
@@ -1,18 +1,28 @@
+
 const idCheck = document.getElementById("idcheck");   
 const psCheck = document.getElementById("passwordcheck"); 
+const joinClick = document.getElementById("submitclick");
+
 
 
 function checkId(){
     alert("user id check!!");
-}
+};
 
 idCheck.addEventListener("click",checkId);
   
 
 function checkPs(){
     alert("user password check!!");
-}
+};
 
 psCheck.addEventListener("click",checkPs);
   
+
+
+function submitClick(){
+    alert("submit complete!");
+};
+
+joinClick.addEventListener("click",submitClick);
   
